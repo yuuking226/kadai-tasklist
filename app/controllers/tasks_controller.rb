d41358401c7760ee84b872650e08224f9a607276
yuuki.kanazawa <yuki.kanazawa226@gmail.com>
2020-07-01 09:02:37 +0000

    chang contents

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -43,8 +43,8 @@ class TasksController < ApplicationController
         @task = Task.find(params[:id])
         @task.destroy
 
-    flash[:success] = 'Task は正常に削除されました'
-    redirect_to tasks_url
+        flash[:success] = 'Task は正常に削除されました'
+        redirect_to tasks_url
     end
     
     private
